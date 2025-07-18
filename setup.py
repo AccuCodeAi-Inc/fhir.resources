9eb9d6fc3b006fb1f704cfd63ce2632fdf416679
Eva Valentine <4528461+eva-val@users.noreply.github.com>
2025-07-18 14:50:12 -0700

    Update setup.py
    Signed-off-by: Eva Valentine <4528461+eva-val@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["fhir-core>=1.1.3"]
+requirements = ["git+https://github.com/AccuCodeAi-Inc/fhir.resources.git"]
 if PY_VERSION_9_OR_EARLIER:
     requirements.append("eval-type-backport")
 
